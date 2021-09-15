4e36688f1fad75fc36f9f748d52d709cf17cb0f5
kaycee <Nwobodoe71@gmail.com>
2021-09-15 07:11:22 +0100

    fix reverse string test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function(str) {
-
+const reverseString = function (str) {
+  let reversedString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  return reversedString;
 };
 
 module.exports = reverseString;
