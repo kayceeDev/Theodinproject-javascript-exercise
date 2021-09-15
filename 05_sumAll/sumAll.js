a6bccb8308a3241f8d9e3898a79da7314557faad
kaycee <Nwobodoe71@gmail.com>
2021-09-15 09:34:25 +0100

    fix sumAll test cases to pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function (startIndex, endIndex) {
+  let firstIndex;
+  let secondIndex;
+  let sum = 0;
+  if (
+    typeof startIndex !== "number" ||
+    typeof endIndex !== "number" ||
+    startIndex < 0 ||
+    endIndex < 0
+  ) {
+    return "ERROR";
+  }
 
+  if (startIndex < endIndex) {
+    firstIndex = startIndex;
+    secondIndex = endIndex;
+  } else {
+    firstIndex = endIndex;
+    secondIndex = startIndex;
+  }
+  for (let i = firstIndex; i <= secondIndex; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
+
+console.log(sumAll(4, 1));
 
 module.exports = sumAll;
