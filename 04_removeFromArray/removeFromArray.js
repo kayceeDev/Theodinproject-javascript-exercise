089b8c32f74c2e3c48d5a4dd05f040edafdb4d1b
kaycee <Nwobodoe71@gmail.com>
2021-09-15 08:25:51 +0100

    Fix remove array test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let itemIndex = 0;
+  let itemTwoIndex = 0;
 
+  //   if (!arr.includes(item1)) {
+  //     arr.splice(itemOneIndex, 0);
+  //   } else {
+  //     itemOneIndex = arr.findIndex((item) => item === item1);
+  //     arr.splice(itemOneIndex, 1);
+  //   }
+
+  //   if (!arr.includes(item2)) {
+  //     arr.splice(itemTwoIndex, 0);
+  //   } else {
+  //     itemTwoIndex = arr.findIndex((item) => item === item2);
+  //     arr.splice(itemTwoIndex, 1);
+  //   }
+  //   return arr;
+  let newArray = [...args];
+  for (let index = 0; index < newArray.length; index++) {
+    let itemToRemove = newArray[index];
+    if (arr.includes(itemToRemove)) {
+      itemIndex = arr.findIndex((item) => item === itemToRemove);
+      arr.splice(itemIndex, 1);
+    }
+  }
+  return arr;
 };
 
 module.exports = removeFromArray;
