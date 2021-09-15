5700b8d6ac3f8efbc33d5c6a8fdb1e4de0d989da
kaycee <Nwobodoe71@gmail.com>
2021-09-15 12:55:27 +0100

    fix tempConversion test cases to pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(f) {
+// ([°F] − 32) × 5⁄9
 
+celcius = Math.round(((f - 32) * (5/9)) * 10 ) / 10 
+return celcius
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+// [°C] × 9⁄5 + 32
+fahrenheit = Math.round((c * (9/5) + 32) *10 ) /10
+return fahrenheit
 };
 
 module.exports = {
