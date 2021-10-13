4b24cf1ac67e69226f098172d00a0ef1c0dc18a7
kaycee <Nwobodoe71@gmail.com>
2021-10-13 13:22:07 +0100

    Add findtheoldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+const newArr= arr.map(a=>{
+    if(!('yearOfDeath' in a)){
+        a['yearOfDeath'] = new Date().getFullYear()
+        return a
+   }else{
+       return a
+   }
+}).sort((a,b)=>{
+    const lastGuy = a.yearOfDeath - a.yearOfBirth
+    const nextGuy = b.yearOfDeath - b.yearOfBirth
+    return lastGuy > nextGuy ? -1 : 1
+})
 
+return newArr[0]
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+
+  console.log(findTheOldest(people))
 
 module.exports = findTheOldest;
