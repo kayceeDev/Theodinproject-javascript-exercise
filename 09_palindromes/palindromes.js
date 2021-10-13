cea3be8e05e30d5c4cd2b153771674b0b4fffd14
kaycee <Nwobodoe71@gmail.com>
2021-10-13 02:14:28 +0100

    palindromes exercise solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // var reverseString=""; // initialize every string to ""
 
+  //   formatted = temp.replace(/[^A-Za-z0-9]/g, '');
+  let formatted = str
+    .toLowerCase()
+    .replace(/[^a-zA-Z ]/g, "")
+    .split(" ")
+    .join("");
+  let reversedString = formatted.split('').reverse().join('');
+
+  return formatted === reversedString;
 };
 
 module.exports = palindromes;
